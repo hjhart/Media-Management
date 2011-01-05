edb6f5894677a9d2e03db3a8a2724d1080804988
James Hart <jhart@rpxcorp.com>
2011-01-04 19:35:43 -0800

    First commit
    Signed-off-by: James Hart <jhart@rpxcorp.com>

---
+++ update_movie_names.rb
@@ -0,0 +1,229 @@
+#!/usr/bin/env ruby
+
+# In order for watir to work, you need to install firewatir using these instructions:
+# http://watir.com/installation/#mac
+# After installation, you need to start firefox with javascript shell enabled
+# To do this, you need to start firefox with the -jssh flag
+# I set up an alias in ~/.profile below
+# alias firewatir="nohup /Applications/Firefox.app/Contents/MacOS/firefox-bin -jssh &"
+
+# Thanks to Zsolt Török on Stack Overflow for the illegal charactesr list. 
+
+# TODO: Get the project ported over to mechanize?
+
+# Debug mode changes no file names, just runs the tests to let you know what they WOULD change to
+@debug = false
+# Log file
+@output_file = '/tmp/movie_rename_output.txt'
+
+ROOT_DIR = "/Volumes/Media/Downloads"
+#ROOT_DIR = "/Volumes/Media/Video/Movies"
+#ROOT_DIR = "/Users/jhart/Movies"
+#ROOT_DIR = "/Users/jhart/Sites/media_management/dummy"
+SUBSTITUTIONS = {/[\(\)\.-_\[\]\}\{]/=>' '} # Punctuation substitions for the movie title
+MOVIE_FILE_EXTENSIONS = [/.avi$/, /.mkv$/, /.mp4$/] # Video extensions whose names will be changed - update if I missed some
+ILLEGAL_CHARACTERS = [ '/', /\n/, /\r/, /\t/, /\0/, /\f/, '`', '?', '*', '\\', '<', '>', '|', '\"', ':']
+BLACKLIST_TERMS = [/\.avi$/,/\.mkv$/,/\.mp4$/,/\d{3,4}p/,/dvdrip/i,/ac3/,/eng/i,/xvid/i,
+  /fxm/i,/fxg/i,/axxo/i,/ppvrip/i,/www\..*\.(com|org|net)/,/download/i,/iwanna/i,
+  /extratorrentrg/,/torrent/,/vice/i,/nydic/i,/maxspeed/i,/torentz/,/3xforum/,/usabit/,
+  /amiable/,/FxM/i,/aAF/i,/AFO/i,/AXIAL/i,/UNiVERSAL/i,/PFa/i,/SiRiUS/i,/Rets/i,
+  /BestDivX/i,/NeDiVx/i,/ESPiSE/i,/iMMORTALS/i,/QiM/i,/QuidaM/i,/COCAiN/i,/DOMiNO/i,
+  /JBW/i,/LRC/i,/WPi/i,/NTi/i,/SiNK/i,/HLS/i,/HNR/i,/iKA/i,/LPD/i,/DMT/i,/DvF/i,/IMBT/i,
+  /LMG/i,/DiAMOND/i,/D0PE/i,/NEPTUNE/i,/SAPHiRE/i,/PUKKA/i,/FiCO/i,/aXXo/i,/VoMiT/i,/ViTE/i,
+  /ALLiANCE/i,/mVs/i,/XanaX/i,/FLAiTE/i,/PREVAiL/i,/CAMERA/i,/VH-PROD/i,/BrG/i,/replica/i,
+  /FZERO/i,/multisub/,/fps/,/kbps/,/wunseedee/,/ppvrip/,/dvdscr/]
+  
+# Intialize the logging variables
+@failures = []
+@messages = []
+@successes = {}
+
+def self.initialize_watir()
+  require 'rubygems'
+  require 'firewatir'
+  
+  b = Watir::Browser.new
+
+  return b
+end
+
+def self.movie_file?(filename)
+  # check if it's a movie file first
+  movie_file = false
+  
+  MOVIE_FILE_EXTENSIONS.each do |ext|
+    if(filename[ext])
+      movie_file = true
+    end
+  end
+  
+  if(filename[/sample/i])
+    movie_file = false
+  end
+  
+  return movie_file
+end
+
+def self.clean_name_for_search(filename)
+  
+  filename = filename.downcase
+  
+  # Let's try removing everything after the 4 digit date
+  match = filename.scan /.*[\[\{\(]?\d{4}[\]\}\)]?/
+  if(match)
+    filename = match[0] unless !match[0].kind_of? String
+    @messages << "Movie name possibly detected. #{filename}"
+  end
+  
+  BLACKLIST_TERMS.each { |term|
+    if(filename[term])
+      filename = filename.gsub(term, '')
+    end
+  }
+  
+  SUBSTITUTIONS.each do |k,v|
+    if(filename[k])
+      filename = filename.gsub(k,v)
+    end
+  end
+  
+  return filename
+end
+
+def self.im_feeling_lucky_search(filename)
+  #TODO: URL parsing correctly.
+  puts "Searching for #{filename}"
+  search_string = CGI::escape(filename + " site:imdb.com")
+  uri = "http://www.google.com/#q=#{search_string}&btnI"
+  @b.goto uri
+  # Wait for the redirect from I'm Feeling Lucky redirect
+  sleep 1 until !@b.title[/google/i]
+  # Is the first link a IMDB page?
+  if(@b.title[/imdb/i])
+    if @b.title[/google search/i]
+      @failures << "IMDB Search failed on " + filename
+      puts "We're stuck in some weird bug of firewatir. This probably needs to be fixed."
+      sleep 3 
+      return nil
+    else
+      return @b.title.gsub(/ - imdb/i, '')
+    end
+  else
+    # TODO: See if it's worth my time to follow through here, or just hand rename them
+    puts "IMDB Search failed – Title was #{@b.title}"
+    @failures << "IMDB Search failed on " + filename
+    return nil
+  end
+end
+
+# This processes an individual file, and passes a filename to clean it up
+def self.process_file(filename, directory=nil)
+  # If it's already in good format, just return okay.
+  
+  original_filename = filename
+  
+  if(filename[/(.*) \(\d\d\d\d\)\.\w\w\w/])
+    puts "Already named properly with #{filename}"
+    @messages << "Already named properly with #{filename}"
+    return nil
+  end
+  if(movie_file? filename)
+    # Grab extension, clean filename
+    extension = filename[filename.rindex('.'),filename.length]
+    filename = clean_name_for_search(filename)
+    
+    # Search on Google to get the title name
+    rename_to = im_feeling_lucky_search(filename)
+
+    if(rename_to)
+      directory = File::SEPARATOR + directory rescue ""
+   
+      # To be safe cross-platform, we remove all potentially invalid characters in filenames
+      ILLEGAL_CHARACTERS.each { |char|
+        rename_to.gsub!(char, '-')
+      }
+            
+      if(!@debug)
+        begin
+          # Move the file to it's new name (and up a directory if it was one directory deep)
+          File.rename(ROOT_DIR + directory + File::SEPARATOR + original_filename, ROOT_DIR + File::SEPARATOR + rename_to + extension)
+          @successes[filename] = rename_to + extension
+          puts "#{directory + File::SEPARATOR + filename} => #{rename_to + extension}"
+        rescue Exception => e
+          puts "Failed - " + e
+          @failures << filename
+        end
+      else
+        @successes[filename] = rename_to + extension + "(DEBUG)"
+        puts "#{directory + File::SEPARATOR + filename} => #{rename_to + extension}"
+      end
+      
+    end
+  else
+    @messages << "#{filename} was not a recognized movie file"
+  end
+end
+
+
+## MAIN PROGRAM RUNS
+
+@b = initialize_watir()
+require 'cgi' # For URL encoding
+require 'ftools' # To rename files
+
+
+
+d = Dir.new(ROOT_DIR)
+
+d.each { |filename|
+  puts "Processing #{filename}"
+	if(filename[/^\./])
+		#puts "Starts with a period. Skipping #{filename}"
+	else
+	  # Test for file (as opposed to directory)
+	  if(File.ftype(ROOT_DIR + File::SEPARATOR + filename) == 'file')
+	    process_file(filename)
+    else
+      directory = filename
+      d1 = Dir.new(ROOT_DIR + File::SEPARATOR + filename)
+      d1.each do |folder_filename|
+        if(File.ftype(ROOT_DIR + File::SEPARATOR + directory + File::SEPARATOR + folder_filename) == 'file')
+          process_file(folder_filename, directory)
+        else
+          # Too deep. No need to enter two folders deep. (If you do need to, start here)
+        end
+      end
+    end
+	end
+}
+
+# OUTPUT RESULTS TO A FILE
+
+f = File.new(@output_file,'w')
+f.puts
+f.puts "======================"
+f.puts "Failures"
+f.puts "======================"
+@failures.each { |failure|
+  f.puts failure
+} 
+f.puts
+f.puts "======================"
+f.puts "Messages"
+f.puts "======================"
+@messages.each { |message|
+  f.puts message
+}
+f.puts
+f.puts "======================"
+f.puts "Successes"
+f.puts "(The below files were renamed from the left filename to the right)"
+f.puts "If you're in debug mode, the files didn't actually change names"
+f.puts "======================"
+@successes.each { |k, v|
+  f.puts k + "=>" + v
+}
+  
+f.close
+  
+#END PROGRAM
