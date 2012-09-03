f8a621c3dfb01c16234ffd8d80eebb8a9f336b3e
James Hart <hjhart@gmail.com>
2012-09-03 13:53:44 -0700

    Checkin to github.

--- update_movie_names.rb
+++ update_movie_names.rb
@@ -12,185 +12,192 @@
 # TODO: Get the project ported over to mechanize?
 
 # Debug mode changes no file names, just runs the tests to let you know what they WOULD change to
-@debug = true
+@debug = false
 # Log file
 @output_file = '/tmp/movie_rename_output.txt'
 
-#ROOT_DIR = "/Volumes/Media/Downloads"
+#ROOT_DIR = "/Volumes/Media/Video/Rename"
+ROOT_DIR = "/Volumes/Media/Downloads"
+DESTINATION_DIR = "/Volumes/Media/Video/Movies"
 #ROOT_DIR = "/Volumes/Media/Video/Movies"
-ROOT_DIR = "/Users/jhart/Movies"
+#ROOT_DIR = "/Users/jhart/Movies"
 #ROOT_DIR = "/Users/jhart/Sites/media_management/dummy"
 SUBSTITUTIONS = {/[\(\)\.\-_\[\]\}\{]/=>' '} # Punctuation substitions for the movie title
+TV_SERIES_DETECTION = /(s\d{1,2} ?e?\d{2}|\d+x\d+|\b\d[012]\d\b)/i
 MOVIE_FILE_EXTENSIONS = [/.avi$/, /.mkv$/, /.mp4$/] # Video extensions whose names will be changed - update if I missed some
 ILLEGAL_CHARACTERS = [ '/', /\n/, /\r/, /\t/, /\0/, /\f/, '`', '?', '*', '\\', '<', '>', '|', '\"', ':']
 BLACKLIST_TERMS = [/\.avi$/,/\.mkv$/,/\.mp4$/,/\d{3,4}p/,/dvdrip/i,/ac3/,/eng/i,/xvid/i,
-  /fxm/,/fxg/,/axxo/,/ppvrip/,/www\..*\.(com|org|net)/,/download/i,/iwanna/i,
-  /extratorrentrg/,/torrent/,/vice/,/nydic/,/maxspeed/i,/torentz/,/3xforum/,/usabit/,
-  /amiable/,/FxM/,/aAF/,/AFO/,/AXIAL/,/UNiVERSAL/,/PFa/,/SiRiUS/,/Rets/,
-  /BestDivX/,/NeDiVx/,/ESPiSE/,/iMMORTALS/,/QiM/,/QuidaM/,/COCAiN/,/DOMiNO/,
-  /JBW/,/LRC/,/WPi/,/NTi/,/SiNK/,/HLS/,/HNR/,/iKA/,/LPD/,/DMT/,/DvF/,/IMBT/,
-  /LMG/,/DiAMOND/,/D0PE/,/NEPTUNE/,/SAPHiRE/,/PUKKA/,/FiCO/,/aXXo/,/VoMiT/,/ViTE/,
-  /ALLiANCE/,/mVs/,/XanaX/,/FLAiTE/,/PREVAiL/,/CAMERA/,/VH-PROD/,/BrG/,/replica/,
-  /FZERO/,/multisub/,/fps/,/kbps/,/wunseedee/,/ppvrip/,/dvdscr/]
-  
+/fxm/,/fxg/,/axxo/,/ppvrip/,/www\..*\.(com|org|net)/,/download/i,/iwanna/i,
+/extratorrentrg/,/torrent/,/vice/,/nydic/,/maxspeed/i,/torentz/,/3xforum/,/usabit/,
+/amiable/,/FxM/,/aAF/,/AFO/,/AXIAL/,/UNiVERSAL/,/PFa/,/SiRiUS/,/Rets/,
+/BestDivX/,/NeDiVx/,/ESPiSE/,/iMMORTALS/,/QiM/,/QuidaM/,/COCAiN/,/DOMiNO/,
+/JBW/,/LRC/,/WPi/,/NTi/,/SiNK/,/HLS/,/HNR/,/iKA/,/LPD/,/DMT/,/DvF/,/IMBT/,
+/LMG/,/DiAMOND/,/D0PE/,/NEPTUNE/,/SAPHiRE/,/PUKKA/,/FiCO/,/aXXo/,/VoMiT/,/ViTE/,
+/ALLiANCE/,/mVs/,/XanaX/,/FLAiTE/,/PREVAiL/,/CAMERA/,/VH-PROD/,/BrG/,/replica/,
+/FZERO/,/multisub/,/fps/,/kbps/,/wunseedee/,/ppvrip/,/dvdscr/,/AMAIBLE/,/XMXG/,/SUBMERGE/,/ExtraTorrentRG/,/WBZ/]
+
 # Intialize the logging variables
 @failures = []
 @messages = []
 @successes = {}
 def self.initialize_mechanize()
-  require 'rubygems'
-  require 'mechanize'
-  
-  @agent = Mechanize.new { |a|
-     a.user_agent_alias = 'Mac Safari'
-     a.follow_meta_refresh = true
-  }
+require 'rubygems'
+require 'mechanize'
+
+@agent = Mechanize.new { |a|
+a.user_agent_alias = 'Mac FireFox'
+a.follow_meta_refresh = true
+}
 end
 
 def self.initialize_watir()
-  require 'rubygems'
-  require 'firewatir'
-  
-  b = Watir::Browser.new
+require 'rubygems'
+require 'firewatir'
 
-  return b
+b = Watir::Browser.new
+
+return b
 end
 
 def self.movie_file?(filename)
-  # check if it's a movie file first
-  movie_file = false
-  
-  MOVIE_FILE_EXTENSIONS.each do |ext|
-    if(filename[ext])
-      movie_file = true
-    end
-  end
-  
-  if(filename[/s\d\de\d\d/i])
-    movie_file = false
-    @messages << "TV Show detected on #{filename}"
-  end
-  
-  if(filename[/sample/i])
-    movie_file = false
-    @messages << "Sample movie detected on #{filename}"
-    
-  end
-  
-  return movie_file
+# check if it's a movie file first
+movie_file = false
+
+MOVIE_FILE_EXTENSIONS.each do |ext|
+if(filename[ext])
+movie_file = true
+end
+end
+
+if(filename[TV_SERIES_DETECTION])
+movie_file = false
+@messages << "TV Show detected on #{filename}"
+end
+
+if(filename[/sample/i])
+movie_file = false
+@messages << "Sample movie detected on #{filename}"
+
+end
+
+return movie_file
 end
 
 def self.clean_name_for_search(filename)
-  # remove the extension
-  filename = File.basename(filename, '.*')
-  
-  # Let's try removing everything after the 4 digit date
-  match = filename[/.*[\[\{\(]?\d{4}[\]\}\)]?/]
+# remove the extension
+filename = File.basename(filename, '.*')
 
-  if(match)
-    filename = match[0] unless !match[0].kind_of? String
-    @messages << "Movie name possibly detected. #{filename}"
-  else 
-    @messages << "No movie name detected, blacklisting terms on #{filename}"
-    
-    BLACKLIST_TERMS.each { |term|
-      if(filename[term])
-        filename = filename.gsub(term, '')
-      end
-    }
-  end
-  
-  SUBSTITUTIONS.each do |k,v|
-    if(filename[k])
-      filename = filename.gsub(k,v)
-    end
-  end
-  
-  filename
+# Let's try removing everything after the 4 digit date
+match = filename.match(/.*[\[\{\(]?\d{4}[\]\}\)]?/)
+
+if(match)
+filename = match[0] unless !match[0].kind_of? String
+@messages << "Movie name possibly detected. #{filename}"
+else 
+@messages << "No movie name detected, blacklisting terms on #{filename}"
+
+BLACKLIST_TERMS.each { |term|
+if(filename[term])
+filename = filename.gsub(term, '')
+end
+}
+end
+
+SUBSTITUTIONS.each do |k,v|
+if(filename[k])
+filename = filename.gsub(k,v)
+end
+end
+
+filename
 end
 
 def self.fetch_movie_info(movie_title)
-  puts "Searching for " + movie_title
-   uri = "http://www.google.com/"
-   page = @agent.get(uri)
+puts "Searching for " + movie_title
+uri = "http://www.google.com/"
+page = @agent.get(uri)
 
-   search_form = page.form_with(:name => "f")
-   search_form.field_with(:name => "q").value = movie_title + " site:imdb.com"
-   search_results = @agent.submit(search_form)
+search_form = page.form_with(:name => "gbqf")
+search_form.field_with(:name => "q").value = movie_title + " site:imdb.com"
+search_results = @agent.submit(search_form)
 
-   title = nil
+title = nil
 
-   search_results.links.each do |link|
-     if(link.href[/imdb\.com\/title\//])
-       movie_page = link.click
-       if(movie_page.title[/(.*)\(\d{4}\) - IMDb/])
-         title = movie_page.title[/(.*\(\d{4}\))/]
-         break
-       end
-     end
-   end
+search_results.links.each do |link|
+if link.href.nil? 
+puts "Filename is probably shit. Skipping."; break; 
+end
 
-   return title
+if(link.href[/imdb\.com\/title\//])
+movie_page = link.click
+if(movie_page.title[/(.*)\(\d{4}\) - IMDb/])
+ title = movie_page.title[/(.*\(\d{4}\))/]
+ break
+end
+end
+end
+
+return title
 end
 
 def self.im_feeling_lucky_search(filename)
-  #TODO: URL parsing correctly.
-  puts "Searching for #{filename}"
-  search_string = CGI::escape(filename + " site:imdb.com")
-  uri = "http://www.google.com/#q=#{search_string}&btnI"
-  @b.goto uri
-  # Wait for the redirect from I'm Feeling Lucky redirect
-  sleep 1 until !@b.title[/google/i]
-  # Is the first link a IMDB page?
-  if(@b.title[/imdb/i])
-    if @b.title[/google search/i]
-      @failures << "IMDB Search failed on " + filename
-      puts "We're stuck in some weird bug of firewatir. This probably needs to be fixed."
-      sleep 3 
-      return nil
-    else
-      return @b.title.gsub(/ - imdb/i, '')
-    end
-  else
-    # TODO: See if it's worth my time to follow through here, or just hand rename them
-    puts "IMDB Search failed – Title was #{@b.title}"
-    @failures << "IMDB Search failed on " + filename
-    return nil
-  end
+#TODO: URL parsing correctly.
+puts "Searching for #{filename}"
+search_string = CGI::escape(filename + " site:imdb.com")
+uri = "http://www.google.com/#q=#{search_string}&btnI"
+@b.goto uri
+# Wait for the redirect from I'm Feeling Lucky redirect
+sleep 1 until !@b.title[/google/i]
+# Is the first link a IMDB page?
+if(@b.title[/imdb/i])
+if @b.title[/google search/i]
+@failures << "IMDB Search failed on " + filename
+puts "We're stuck in some weird bug of firewatir. This probably needs to be fixed."
+sleep 3 
+return nil
+else
+return @b.title.gsub(/ - imdb/i, '')
+end
+else
+# TODO: See if it's worth my time to follow through here, or just hand rename them
+puts "IMDB Search failed – Title was #{@b.title}"
+@failures << "IMDB Search failed on " + filename
+return nil
+end
 end
 
 # This processes an individual file, and passes a filename to clean it up
 def self.process_file(filename, directory=nil)
-  # If it's already in good format, just return okay.
-  
-  original_filename = filename
-  
-  if(filename[/(.*) \(\d\d\d\d\)\.\w\w\w/])
-    puts "Already named properly with #{filename}"
-    @messages << "Already named properly with #{filename}"
-    return nil
-  end
-  if(movie_file? filename)
-    # Grab extension, clean filename
-    extension = filename[filename.rindex('.'),filename.length]
-    filename = clean_name_for_search(filename)
-    
-    # Search on Google to get the title name
-    #rename_to = im_feeling_lucky_search(filename)
-    rename_to = fetch_movie_info(filename)
+# If it's already in good format, just return okay.
 
-    if(rename_to)
-      directory = File::SEPARATOR + directory rescue ""
-   
-      # To be safe cross-platform, we remove all potentially invalid characters in filenames
-      ILLEGAL_CHARACTERS.each { |char|
-        rename_to.gsub!(char, '-')
-      }
-            
-      if(!@debug)
+original_filename = filename
+
+if(filename[/(.*) \(\d\d\d\d\)\.\w\w\w/])
+puts "Already named properly with #{filename}"
+@messages << "Already named properly with #{filename}"
+return nil
+end
+if(movie_file? filename)
+# Grab extension, clean filename
+extension = filename[filename.rindex('.'),filename.length]
+filename = clean_name_for_search(filename)
+
+# Search on Google to get the title name
+#rename_to = im_feeling_lucky_search(filename)
+rename_to = fetch_movie_info(filename)
+
+if(rename_to)
+directory = File::SEPARATOR + directory rescue ""
+
+# To be safe cross-platform, we remove all potentially invalid characters in filenames
+ILLEGAL_CHARACTERS.each { |char|
+rename_to.gsub!(char, '-')
+}
+    
+if(!@debug)
         begin
           # Move the file to it's new name (and up a directory if it was one directory deep)
-          File.rename(ROOT_DIR + directory + File::SEPARATOR + original_filename, ROOT_DIR + File::SEPARATOR + rename_to + extension)
+          File.rename(ROOT_DIR + directory + File::SEPARATOR + original_filename, DESTINATION_DIR + File::SEPARATOR + rename_to + extension)
           @successes[filename] = rename_to + extension
           puts "#{directory + File::SEPARATOR + filename} => #{rename_to + extension}"
         rescue Exception => e
